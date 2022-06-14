2a68c73b4493a3f08b8997609d4fe1aaf5788bf6
artempelevin <artempelevin@list.ru>
2022-06-14 11:38:15 +0300

    BUG Fixing some problems during testing

--- ria.py
+++ ria.py
@@ -20,13 +20,16 @@ class RiaNewsSite(NewsSite):
 
     def provide_news_pages_for(self, date: datetime) -> list[str]:
         self._required_date = date
-        url_ = self._provide_start_url(date=date)
+        date_in_url = date.strftime('%Y%m%d')
+        url_ = f"https://ria.ru/{date_in_url}"
 
         urls = []
         while self._is_required_date:
             soup_ = self._provide_soup(url=url_)
             urls.extend(self._provide_urls(soup=soup_))
             url_ = self._provide_next_url(soup=soup_)
+            if url_ == 'https://ria.ru':
+                break
         return urls
 
     def provide_last_news_page(self) -> str:
@@ -34,17 +37,11 @@ class RiaNewsSite(NewsSite):
         soup_ = self._provide_soup(url=url_)
         return self._provide_urls(soup=soup_)[0]
 
-    def _provide_start_url(self, date: datetime) -> str:
-        date_in_url = date.strftime('%Y%m%d')
-        start_url = f"https://ria.ru/{date_in_url}/"
-        soup = self._provide_soup(url=start_url)
-        next_url_ = f"https://ria.ru{soup.find(name='div', class_='list-more')['data-url']}"
-        soup = self._provide_soup(url=next_url_)
-        return f"https://ria.ru{soup.find(name='div', class_='list-items-loaded')['data-next-url']}"
-
     @staticmethod
     def _provide_soup(url: str) -> BeautifulSoup:
-        request = requests.get(url=url)
+        request = requests.get(url=url, headers={
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                          'Chrome/74.0.3729.169 Safari/537.36'})
         return BeautifulSoup(request.text, 'lxml')
 
     def _provide_urls(self, soup: BeautifulSoup) -> list[str]:
@@ -54,8 +51,7 @@ class RiaNewsSite(NewsSite):
             self._update_date(text_with_date=text_with_date_)
             if not self._is_required_date:
                 break
-
-            urls.append(item.find(name='a', class_='list-item__image')['href'])
+            urls.append(item.find(name='a', class_='list-item__title color-font-hover-only')['href'])
         return urls
 
     def _update_date(self, text_with_date: str) -> None:
@@ -68,4 +64,7 @@ class RiaNewsSite(NewsSite):
 
     @staticmethod
     def _provide_next_url(soup: BeautifulSoup) -> str:
-        return f"https://ria.ru{soup.find(name='div', class_='list-items-loaded')['data-next-url']}/"
+        if soup.find(name='div', class_='list-more'):       # Human-readble page
+            return f"https://ria.ru{soup.find(name='div', class_='list-more')['data-url']}"
+        else:   # Loading the pagination content
+            return f"https://ria.ru{soup.find(name='div', class_='list-items-loaded')['data-next-url']}"
